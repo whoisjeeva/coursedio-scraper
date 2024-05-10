07ab5eb382fcd9de2d6c5d35cfecc94e330e0278
whoisjeeva <mytellee@gmail.com>
2024-05-10 16:05:54 +0530

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import VideoFileClip
-import os
-
-
-videoClip = VideoFileClip("v.mp4")
-videoClip.write_videofile("v.webm")
-videoClip = VideoFileClip("v.webm")
-os.remove("v.mp4")
-videoClip.write_videofile("v.mp4")
